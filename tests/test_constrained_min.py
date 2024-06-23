0544f17bfd8005c046c351f6e4c12979ac7e12fc
RotemGat <rotemg8@gmail.com>
2024-06-23 19:49:53 +0300

    added tests

--- tests/test_constrained_min.py
+++ tests/test_constrained_min.py
@@ -1,12 +1,29 @@
 from unittest import TestCase
+from constrained_min import InteriorPointSolver
+from examples import qp, qp_ineq_constraints, lp, lp_ineq_constraints
+from utils import *
 
 
 class TestConstrainedMin(TestCase):
-    def setUp(self) -> None:
-        pass
+
+    def base_func(self, func, ineq_constraints, eq_mat, eq_r, init_x0, name, plot_dim):
+        solver = InteriorPointSolver()
+        x, f = solver.minimize(func, ineq_constraints, eq_mat, eq_r, init_x0)
+        print(f'Final candidate: {x}')
+        print(f'Objective function of final candidate: {f}')
+        print(f'Inequality constraints of final candidate: {[con(x) for con in ineq_constraints]}')
+        final_path = solver.x_history
+
+        plot_function_values(f_values_list=[solver.f_history], labels=[name], example_name=name)
+        if plot_dim == '3D':
+            plot_feasible_regions_3d(final_path, name)
+        else:
+            plot_feasible_set_2d(final_path, name)
 
     def test_qp(self):
-        pass
+        self.base_func(qp, qp_ineq_constraints, np.array([1, 1, 1]).reshape(1, 3), 0, QP_INIT_XO, 'QP', '3D')
 
     def test_lp(self):
-        pass
+        self.base_func(lp, lp_ineq_constraints, np.array([]), np.array([]), LP_INIT_XO, 'LP', '2D')
+
+
