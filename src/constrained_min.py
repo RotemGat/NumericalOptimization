4ed9942ffe25e42f243cb7210f86f41117effb04
RotemGat <rotemg8@gmail.com>
2024-06-23 20:44:17 +0300

    fixed backtracking

--- src/constrained_min.py
+++ src/constrained_min.py
@@ -15,7 +15,7 @@ class InteriorPointSolver:
         self.x_history = []
         self.f_history = []
 
-    def minimize(self, func, ineq_constraints, eq_constraints_mat, eq_constraints_rhs, x0, outer_loop_max_iter=100, inner_loop_max_iter=10,
+    def minimize(self, func, ineq_constraints, eq_constraints_mat, eq_constraints_rhs, x0, outer_loop_max_iter=100, inner_loop_max_iter=15,
                  epsilon=1e-6, obj_tol=1e-8, param_tol=1e-6):
         """ Minimize the function func subject to the list of inequality constraints specified by ineq_constraints and to the affine
         equality constraints specified by eq_constraints_mat and the right hand side vector eq_constraints_rhs """
@@ -24,18 +24,19 @@ class InteriorPointSolver:
         t = self.init_t
         success = False
         phi, phi_tag, phi_hessian = self._get_log_barrier(x, ineq_constraints)
-        f, g, h = self.barrier_func(x, func, t, phi, phi_tag, phi_hessian)
+        f, g, h = self._barrier_func(x, func, t, phi, phi_tag, phi_hessian)
 
         outer_i = 0
+        self._save_history(outer_i, x, func)
         while outer_i < outer_loop_max_iter and len(ineq_constraints) / t > epsilon:
             for inner_iteration in range(inner_loop_max_iter):
                 p = self._solve_kkt_system(g, h, eq_constraints_mat, eq_constraints_rhs)
 
-                alpha = backtracking(self.alpha, self.c, self.backtracking_const, func, x, f, p, g)
+                alpha = self._backtracking(func, x, f, p, ineq_constraints)
                 x_new = x + p * alpha
 
                 phi, phi_tag, phi_hessian = self._get_log_barrier(x_new, ineq_constraints)
-                f_new, g_new, h_new = self.barrier_func(x_new, func, t, phi, phi_tag, phi_hessian)
+                f_new, g_new, h_new = self._barrier_func(x_new, func, t, phi, phi_tag, phi_hessian)
                 lambda_squared = 0.5 * ((p.T @ (h_new @ p)) ** 0.5)
 
                 # np.abs(f_new - f) < obj_tol or np.linalg.norm(x - x_new) < param_tol
@@ -45,7 +46,7 @@ class InteriorPointSolver:
                     break
                 x, f, g, h = x_new, f_new, g_new, h_new
 
-            self._save_history(outer_i, x, f)
+            self._save_history(outer_i, x, func)
             t *= self.mu
             if success:
                 break
@@ -53,10 +54,11 @@ class InteriorPointSolver:
 
         return x, func(x)[0]
 
-    def _save_history(self, outer_i: int, x0: np.ndarray, fx: np.ndarray):
+    def _save_history(self, outer_i: int, x0: np.ndarray, func):
         self.x_history.append(x0)
-        self.f_history.append(fx)
-        print(f'Outer Iteration: i={outer_i} , Current location: x_i={x0}, Objective value: {fx}')
+        f = func(x0)[0]
+        self.f_history.append(func(x0)[0])
+        print(f'Outer Iteration: i={outer_i} , Current location: x_i={x0}, Objective value: {f}')
 
     @staticmethod
     def _solve_kkt_system(g, h, eq_constraints_mat, eq_constraints_rhs):
@@ -83,19 +85,26 @@ class InteriorPointSolver:
 
         for func in ineq_constraints:
             f, g, h = func(x, True)
-            try:
-                phi -= math.log(-f)
-            except ValueError:
-                pass
+            phi -= math.log(-f)
             phi_tag -= g / f
             phi_hessian += np.outer(g / f, g / f) / (f ** 2) - h / f
 
         return phi, phi_tag, phi_hessian
 
     @staticmethod
-    def barrier_func(x, func, t, phi, phi_tag, phi_hessian, hessian=True):
+    def _barrier_func(x, func, t, phi, phi_tag, phi_hessian, hessian=True):
         f, g, h = func(x, hessian)
         f_barrier = t * f + phi
         g_barrier = t * g + phi_tag
         h_barrier = t * h + phi_hessian
         return f_barrier, g_barrier, h_barrier
+
+    def _backtracking(self, func, x, f, p, ineq_constraints):
+        alpha, c, rho = self.alpha, self.c, self.backtracking_const,
+        constraints_met = False
+        old_alpha = math.inf
+        while (func(x + alpha * p, False)[0] > f + c * alpha * p.dot(p) or not constraints_met) and (abs(alpha - old_alpha) > 1e-1 or old_alpha == math.inf):
+            old_alpha = alpha
+            alpha = rho * old_alpha
+            constraints_met = all([func(x + alpha * p, False)[0] < 0 for func in ineq_constraints])
+        return alpha
